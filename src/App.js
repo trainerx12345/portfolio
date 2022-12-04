19fa7dfaeb64cd2f3181c86cb722160b20443971
Sam <sambusinesstransac@gmail.com>
2022-12-04 12:55:06 +0800

    Feat: fix changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
 	return (
 		<div>
-			<div id='planetary'>
-				<div class='circle'></div>
-				<div id='planetary-container'></div>
-			</div>
+		
 		</div>
 	);
 }
