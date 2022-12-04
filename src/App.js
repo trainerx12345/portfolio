f11bf2b968eb0b950de5e7e265ccbfd24a38f096
Samuel Corpuz <xdsamuelcorpuz@gmail.com>
2022-12-04 12:07:16 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<div id='planetary'>
+				<div class='circle'></div>
+				<div id='planetary-container'></div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
